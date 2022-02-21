a15a6fa6adc0a7a0cae68bac8b8b47cfb82d34fd
Chao Zhang <chao.zhang@instacart.com>
2022-02-21 11:03:42 -0800

    Configure compile to depends on code generation

--- example-jvm/build.gradle.kts
+++ example-jvm/build.gradle.kts
@@ -24,6 +24,7 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+compileKotlin.dependsOn(tasks.xuke)
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
