266400af597a9e06abeb9a6b97a1e49d86bf2a2d
Chao Zhang <chao.zhang@instacart.com>
2022-02-21 10:40:58 -0800

    Fix build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ tasks.register("reformatAll") {
 tasks.register("preMerge") {
     description = "Runs all the tests/verification tasks on both top level and included build."
 
-    dependsOn(":example:check")
+    dependsOn(":example-jvm:check")
     dependsOn(gradle.includedBuild("plugin-build").task(":plugin:check"))
     dependsOn(gradle.includedBuild("plugin-build").task(":plugin:validatePlugins"))
 }
